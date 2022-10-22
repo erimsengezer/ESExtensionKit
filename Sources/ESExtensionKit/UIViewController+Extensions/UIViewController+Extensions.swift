d11094c22647f5c027a54dadcb1413d38964b800
erimsengezer <erimsengezer@gmail.com>
2022-10-23 01:14:44 +0300

    Add UINavigationController+Extensions & Add UIViewController navigation extensions

--- Sources/ESExtensionKit/UIViewController+Extensions/UIViewController+Extensions.swift
+++ Sources/ESExtensionKit/UIViewController+Extensions/UIViewController+Extensions.swift
@@ -9,6 +9,12 @@ import UIKit
 
 extension UIViewController {
     
+    public var appDelegate: AppDelegate {
+        get {
+            return UIApplication.shared.delegate as! AppDelegate
+        }
+    }
+    
     public func showToast(message : String, font: UIFont, toastColor: UIColor = UIColor.white,
                    toastBackground: UIColor = UIColor.black) {
         let toastLabel = UILabel()
@@ -36,5 +42,25 @@ extension UIViewController {
         }) { _ in
             toastLabel.removeFromSuperview()
         }
+    }
+
+    public func presentViewController(_ viewControllerToPresent: UIViewController) {
+        self.present(viewControllerToPresent, animated: true, completion: nil)
+    }
+
+    public func pushViewControllerToNavigationController(_ viewControllerToPresent: UIViewController, isAnimated: Bool = true) {
+        self.navigationController?.pushViewController(viewControllerToPresent, animated: isAnimated)
+    }
+
+    public func selfDismiss() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    public func selfPopViewController(animated: Bool = true) {
+        self.navigationController?.popViewController(animated: animated)
+    }
+
+    public func selfPopToRootViewController(animated: Bool = false) {
+        self.navigationController?.popToRootViewController(animated: animated)
     }
 }
